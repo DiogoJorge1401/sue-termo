46eba1acf6cdda033efea6b8a636712163952e7b
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-03-01 12:15:23 -0300

    refactor: check if game name exists

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import { App } from '../App';
+import { App } from './App';
 
 describe('Simple working test', () => {
   it('should show the title', () => {
     render(<App />);
 
-    expect(screen.getByText(/Wordle/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/SueTermo/i)
+    ).toBeInTheDocument();
   });
 });
