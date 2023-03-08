c3687fc8c50c41fc4f3411aa4630394c5775ee97
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-03-08 08:44:47 -0300

    refactor: remove StrictComponent

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GameContextProvider } from './context/Game';
@@ -7,9 +6,7 @@ import './index.css';
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
-  <React.StrictMode>
-    <GameContextProvider>
-      <App />
-    </GameContextProvider>
-  </React.StrictMode>
+  <GameContextProvider>
+    <App />
+  </GameContextProvider>
 );
