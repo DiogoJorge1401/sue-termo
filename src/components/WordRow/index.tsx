aaccc6c3f33e8bae0020697f15f0af7d45195885
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-03-08 15:17:09 -0300

    refactor: refactor the WordRow styles

--- src/components/WordRow/index.tsx
+++ src/components/WordRow/index.tsx
@@ -32,7 +32,7 @@ export const WordRow = ({
   return (
     <div
       id="word-row"
-      className={`${rowClass} w-full max-w-xs flex justify-center gap-2 py-2 px-2 mx-auto sm:max-w-sm sm:justify-between sm:gap-0 sm:px-1 md:justify-around lg:justify-center lg:gap-2 xl:gap-3`}
+      className={`${rowClass} w-full max-w-xs flex justify-center gap-2 py-2 px-2 mx-auto sm:justify-around sm:gap-0 sm:px-1 lg:gap-2 lg:py-1 lg:px-2`}
     >
       {wordColumns}
     </div>
