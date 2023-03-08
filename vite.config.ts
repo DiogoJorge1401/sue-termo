adfe2f76298a597b003bd73d994eaf8c857738ce
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-03-08 08:36:41 -0300

    build: change from vitest to jest

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./tests/setup.ts'],
-    reporters: ['verbose'],
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      provider: 'istanbul',
-      lines: 80,
-      functions: 80,
-      branches: 80,
-      statements: 80
-    }
-  }
+  plugins: [react()]
 });
